6ae982faa4f668cdc5893aadf63700e51d3102b4
Avinash Shinde <ajs.ind2@gmail.com>
2025-05-03 15:54:01 +0530

    URL Rechanged

--- src/Component/Pages/Home/Course.jsx
+++ src/Component/Pages/Home/Course.jsx
@@ -8,7 +8,7 @@ function Course() {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        axios.get('https://upskill-servlet.onrender.com/upskill-servlet/get-courses')
+        axios.get('https://upskill-servlet.onrender.com/get-courses')
             .then(response => setDynamicCourses(response.data))
             .catch(error => console.error("Backend error:", error));
 
@@ -34,7 +34,7 @@ function Course() {
                         
                         const isStatic = !!course.Image;
                         const imageUrl = isStatic 
-                            ?course.Image : `https://upskill-servlet.onrender.com/upskill-servlet/get-image?courseId=${course.id}`;
+                            ?course.Image : `https://upskill-servlet.onrender.com/get-image?courseId=${course.id}`;
 
                         return (
                             <div className="col-12 col-sm-6 col-md-3" key={index}>
