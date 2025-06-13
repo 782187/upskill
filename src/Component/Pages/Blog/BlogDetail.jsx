3f2b31727ed5935494a6ba5614003a4a0026ee86
Avinash Shinde <ajs.ind2@gmail.com>
2025-06-13 22:01:27 +0530

    Added blog sub page design

--- src/Component/Pages/Blog/BlogDetail.jsx
+++ src/Component/Pages/Blog/BlogDetail.jsx
@@ -38,32 +38,35 @@ function BlogDetail() {
     }
 
     return (
-        <div className="w-100 min-vh-100 bg-light">
-            {/* Hero Section */}
-            <div className="position-relative">
-                <img
-                    src={blog.image}
-                    alt={blog.title}
-                    className="w-100"
-                    style={{ maxHeight: '450px', objectFit: 'cover' }}
-                />
-                <div className="position-absolute top-50 start-50 translate-middle text-white text-center bg-dark bg-opacity-50 p-4 rounded-4">
-                    <h1 className="display-5 fw-bold">{blog.title}</h1>
-                    <p className="lead">{blog.short_desc}</p>
-                </div>
-            </div>
+        <div className="w-100 bg-light">
+            <div className="container-fluid py-5" style={{ backgroundColor: '#f8f9fa' }}>
+                <div className="row align-items-center">
+                    <div className="col-md-6 text-center text-md-start px-5">
+                        <h1 className="display-5 fw-bold" style={{ color: '#343a40' }}>{blog.title}</h1>
+                        <p className="lead mt-3" style={{ color: '#6c757d' }}>{blog.short_desc}</p>
+                    </div>
 
-            {/* Blog Content */}
-            <div className="container py-5">
-                <div className="row justify-content-center">
-                    <div className="col-lg-10">
-                        <div
-                            className="bg-white p-4 p-md-5 rounded-4 shadow"
-                            style={{ lineHeight: '1.8', fontSize: '1.1rem' }}
-                            dangerouslySetInnerHTML={{ __html: blog.content }}
+                    <div className="col-md-6">
+                        <img
+                            src={blog.image}
+                            alt={blog.title}
+                            className="img-fluid rounded-4 shadow"
+                            style={{ maxHeight: '400px', objectFit: 'cover', width: '100%' }}
                         />
                     </div>
                 </div>
+            </div>
+
+            <div className="container-fluid px-3 px-md-5 py-5">
+                <div className="bg-white p-4 p-md-5 rounded-4 shadow mx-auto"
+                    style={{
+                        lineHeight: '1.8',
+                        fontSize: '1.1rem',
+                        whiteSpace: 'pre-line',
+                        maxWidth: '1200px'
+                    }}
+                    dangerouslySetInnerHTML={{ __html: blog.content }}
+                />
             </div>
         </div>
     );
