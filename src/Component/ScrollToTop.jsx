8b77289087ee6042a3add95c75eb185dc4c77f30
Avinash Shinde <ajs.ind2@gmail.com>
2025-07-28 17:57:03 +0530

    scroll ijihus

--- src/Component/ScrollToTop.jsx
+++ src/Component/ScrollToTop.jsx
@@ -1,14 +1,14 @@
-import { useEffect } from "react";
+import { useLayoutEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     window.scrollTo({
       top: 0,
       left: 0,
-      behavior: 'auto' 
+      behavior: 'auto',
     });
   }, [pathname]);
 
