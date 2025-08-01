0a33eec05593b587c028ca115aac8757f651cb0d
Avinash Shinde <ajs.ind2@gmail.com>
2025-08-01 13:00:27 +0530

    Updated Link Is Not Imported

--- src/Component/Pages/Career.jsx
+++ src/Component/Pages/Career.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { useState } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   const [formData, setFormData] = useState({
